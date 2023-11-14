e821cc4a13fad5c51ce3405c81eb1c183e935cf7
rockchen <rochchen@htsc.com>
2023-11-14 19:26:53 +0800

    update

--- src/main/java/com/hthk/datacenter/controller/DataCenterController.java
+++ src/main/java/com/hthk/datacenter/controller/DataCenterController.java
@@ -2,12 +2,15 @@ package com.hthk.datacenter.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.hthk.fintech.component.AbstractComponent;
+import com.hthk.fintech.model.software.app.AppVersion;
 import com.hthk.fintech.model.web.http.HttpRequest;
 import com.hthk.fintech.model.web.http.HttpResponse;
 import com.hthk.fintech.utils.HttpResponseUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+
+import static com.hthk.fintech.config.FintechStaticData.*;
 
 /**
  * @Author: Rock CHEN
@@ -20,14 +23,17 @@ public class DataCenterController extends AbstractComponent {
     private final static Logger logger = LoggerFactory.getLogger(DataCenterController.class);
 
     @GetMapping(value = "/appVersion")
-    public HttpResponse<?> getAppVersion() {
-        logger.info("{}", "appVersion");
-        return HttpResponseUtils.success(appInfoService.getVersion());
+    public HttpResponse<?> getAppVersion() throws JsonProcessingException {
+
+        AppVersion appVersion = appInfoService.getVersion();
+        logger.info(LOG_WRAP, KW_APP_VERSION, getDefaultObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(appVersion));
+        return HttpResponseUtils.success(appVersion);
     }
 
     @PostMapping(value = "/data")
     public HttpResponse<?> post(@RequestBody HttpRequest request) throws JsonProcessingException {
-        logger.info("{}", getDefaultObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(request));
+
+        logger.info(LOG_WRAP, KW_HTTP_REQUEST, getDefaultObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(request));
 //        return HttpResponseUtils.success(dataQueryService.get(request));
         return HttpResponseUtils.success(appInfoService.getVersion());
     }
