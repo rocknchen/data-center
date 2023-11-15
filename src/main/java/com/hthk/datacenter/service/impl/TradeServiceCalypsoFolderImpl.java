65d6ed03681970e6b37e09d9969290c4776a2a54
rockchen <rochchen@htsc.com>
2023-11-15 19:19:10 +0800

    update

--- src/main/java/com/hthk/datacenter/service/impl/TradeServiceCalypsoFolderImpl.java
+++ src/main/java/com/hthk/datacenter/service/impl/TradeServiceCalypsoFolderImpl.java
@@ -2,7 +2,9 @@ package com.hthk.datacenter.service.impl;
 
 import com.hthk.calypsox.model.trade.ITrade;
 import com.hthk.calypsox.model.trade.datacenter.DataCriteriaTrade;
+import com.hthk.common.utils.FileUtils;
 import com.hthk.datacenter.service.TradeService;
+import com.hthk.fintech.collection.ComparatorFileNameLastDateASC;
 import com.hthk.fintech.config.AppConfig;
 import com.hthk.fintech.exception.ServiceInternalException;
 import com.hthk.fintech.exception.ServiceNotSupportedException;
@@ -11,16 +13,20 @@ import com.hthk.fintech.model.data.datacenter.query.DataSnapshot;
 import com.hthk.fintech.model.data.datacenter.query.SnapshotImageEnum;
 import com.hthk.fintech.model.data.datacenter.service.DataCenterService;
 import com.hthk.fintech.service.basic.AbstractService;
-import com.hthk.fintech.web.http.exp.DefaultAppExceptionHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import static com.hthk.fintech.config.DataCenterStaticData.KW_FOLDER_NAME_TRADE;
-import static com.hthk.fintech.config.DataCenterStaticData.WHITE_LIST_FILE_SUPPORTED_IMAGE_TYPE;
+import static com.hthk.fintech.config.DataCenterStaticData.*;
 import static com.hthk.fintech.config.FintechStaticData.LOG_DEFAULT;
 import static com.hthk.fintech.config.FintechStaticData.LOG_WRAP;
 import static com.hthk.fintech.model.data.datacenter.query.EntityTypeEnum.TRADE;
@@ -36,6 +42,8 @@ public class TradeServiceCalypsoFolderImpl extends AbstractService implements Tr
 
     private static final Logger logger = LoggerFactory.getLogger(TradeServiceCalypsoFolderImpl.class);
 
+    private ComparatorFileNameLastDateASC comparator = new ComparatorFileNameLastDateASC();
+
     @Override
     public List<ITrade> get(DataSnapshot snapshot, DataCriteriaTrade criteria) throws ServiceNotSupportedException, ServiceInternalException {
 
@@ -43,17 +51,17 @@ public class TradeServiceCalypsoFolderImpl extends AbstractService implements Tr
         File tradeSrcFolder = getTradeSrcFolder(appConfig, KW_FOLDER_NAME_TRADE);
         logger.info(LOG_DEFAULT, "src folder", tradeSrcFolder);
 
-        List<File> srcFileList = getSrcFileList(tradeSrcFolder);
-        logger.info(LOG_WRAP, "src file list", srcFileList);
+        String fileNamePrefix = getFileNamePrefix(KW_DATACENTER_FILE_NAME_PREFIX, KW_FILE_NAME_TRADE);
+        logger.info(LOG_DEFAULT, "fileNamePrefix", fileNamePrefix);
+
+        List<File> srcFileList = getSrcFileList(tradeSrcFolder, fileNamePrefix);
+        log(srcFileList);
+
+        List<File> sortedByDateList = sort(srcFileList);
+        log(sortedByDateList);
 
         return null;
-
     }
-
-    private List<File> getSrcFileList(File tradeSrcFolder) {
-        return null;
-    }
-
 
     /**
      * TODO
@@ -83,6 +91,27 @@ public class TradeServiceCalypsoFolderImpl extends AbstractService implements Tr
         return file;
     }
 
+    private List<File> getSrcFileList(File tradeSrcFolder, String fileNamePrefix) {
+
+        List<String> subFileList = FileUtils.getAllSubFileList(tradeSrcFolder);
+        List<String> filterList = filterNoDate(subFileList);
+        return filterList.stream().map(t -> new File(t)).filter(t -> t.getName().startsWith(fileNamePrefix)).collect(Collectors.toList());
+    }
+
+    private List<String> filterNoDate(List<String> subFileList) {
+        return subFileList.stream().filter(t -> {
+            String fileName = new File(t).getName();
+            String simFileName = fileName.substring(0, fileName.indexOf("."));
+            String date = simFileName.substring(simFileName.length() - 8);
+            try {
+                LocalDate.parse(date, DateTimeFormatter.BASIC_ISO_DATE);
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        }).collect(Collectors.toList());
+    }
+
     private void checkIfSupported(DataSnapshot snapshot) throws ServiceNotSupportedException {
         SnapshotImageEnum image = snapshot.getImage();
         boolean isSupported = WHITE_LIST_FILE_SUPPORTED_IMAGE_TYPE.contains(image);
@@ -91,6 +120,24 @@ public class TradeServiceCalypsoFolderImpl extends AbstractService implements Tr
             String errMsg = String.format("Calypso Trade using File as DataCenter, not support %s. Supported white list %s", image, WHITE_LIST_FILE_SUPPORTED_IMAGE_TYPE);
             throw new ServiceNotSupportedException(errMsg);
         }
+    }
+
+    private String getFileNamePrefix(String kwDatacenterFileNamePrefix, String kwFolderNameTrade) {
+        return kwDatacenterFileNamePrefix + "_" + kwFolderNameTrade;
+    }
+
+    private void log(List<File> srcFileList) {
+        if (CollectionUtils.isEmpty(srcFileList)) {
+            return;
+        }
+        String log = srcFileList.stream().map(t -> t.getName()).collect(Collectors.joining("\r\n"));
+        logger.info(LOG_WRAP, "src file list", log);
+    }
+
+    private List<File> sort(List<File> srcFileList) {
+
+        Collections.sort(srcFileList, comparator);
+        return srcFileList;
     }
 
 }
